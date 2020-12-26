cdd37d25e60193caef450103594c856c5173e8e2
redex-2 <redex2mc@gmail.com>
2020-12-26 13:01:24 +0100

    added var for d-h class

--- diffie-hellman.hpp
+++ diffie-hellman.hpp
@@ -1,6 +1,18 @@
 #ifndef diffie_hellman_hpp
 #define diffie_hellman_hpp
 
-
+class diffie_hellman
+{
+	private:
+		unsigned long long p = 0;
+		unsigned long long q = 0;
+		unsigned long long a = 0;
+		unsigned long long A = 0;
+		unsigned long long b = 0;
+		unsigned long long B = 0;
+		unsigned long long secret = 0;
+	public:
+		
+};
 
 #endif
--- testing-main.cpp
+++ testing-main.cpp
@@ -5,5 +5,6 @@
 
 int main()
 {
+	std::cout<<"test"<<std::endl;
 	return 0;
 }
