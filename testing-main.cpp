6bd3e004255f38509a19fb5e5ac6070471ac2aa2
redex-2 <redex2mc@gmail.com>
2020-12-28 14:49:31 +0100

    DES test main

--- testing-main.cpp
+++ testing-main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>
 #include "des.hpp"
 #include "diffie-hellman.hpp"
 #include "rsa.hpp"
 
+des test;
+
 int main()
 {
-	std::cout<<"test"<<std::endl;
+	std::cout<<std::hex<<test.encrypt(0x0123456789ABCDEF, 0x133457799BBCDFF1)<<std::endl;
+	std::cout<<std::hex<<test.decrypt(test.encrypt(0x0123456789ABCDEF, 0x133457799BBCDFF1), 0x133457799BBCDFF1)<<std::endl;
+	//std::cout<<std::hex<<test.encrypt(0x123456ABCD132536, 0xAABB09182736CCDD)<<std::endl;
 	return 0;
 }
