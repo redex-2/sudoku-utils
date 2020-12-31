93fc43a184363affc309f77d6a1c50f77bb773cd
redex-2 <redex2mc@gmail.com>
2020-12-31 23:46:23 +0100

    define and check var size

---
+++ types.hpp
@@ -0,0 +1,12 @@
+#ifndef type
+#define type
+
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long uint64;
+
+static_assert(sizeof(uint16) == 2, "Change type of var for uint16");
+static_assert(sizeof(uint32) == 4, "Change type of var for uint32");
+static_assert(sizeof(uint64) == 8, "Change type of var for uint64");
+
+#endif
