0ad0e40e83cafb731e24fd43ae70c7db983c4759
redex-2 <redex2mc@gmail.com>
2021-01-05 19:24:50 +0100

    construction of database class

--- database.hpp
+++ database.hpp
@@ -1,9 +1,25 @@
 #ifndef database_hpp
 #define database_hpp
 
+#include <libpq-fe.h>
+#include <string>
+#include "../sudoku-utils/types.hpp"
+
 class database
 {
-	
+	private:
+		PGconn *conn;
+		std::string db_name;
+		std::string db_user;
+		std::string db_pass;
+		uint32      db_port;
+		std::string db_addr;
+	public:
+		void setup(std::string db_name, std::string db_user, std::string db_pass, uint32 db_port, std::string db_addr);
+		int connect();
+		
+		
+		int close();
 };
 
 #endif
