5e26b342368a54cf2cebe9ebb60b63d70d3df96e
redex-2 <redex2mc@gmail.com>
2021-01-06 10:15:39 +0100

    added method responsible for prepare data for hash functions

---
+++ hash.hpp
@@ -0,0 +1,12 @@
+#ifndef hash_hpp
+#define hash_hpp
+
+#include "types.hpp"
+
+class _hash
+{
+	protected:
+		void split_message(const char *message, uint64 len, uint32** &out, uint64 &out_len, uint16 block_size);
+};
+
+#endif
