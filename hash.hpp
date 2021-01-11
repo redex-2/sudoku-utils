45fcd2ac3dec0253b82dab63058d442675749d92
redex-2 <redex2mc@gmail.com>
2021-01-11 07:52:29 +0100

    add formatting data method definition for files

--- hash.hpp
+++ hash.hpp
@@ -7,6 +7,7 @@ class _hash
 {
 	protected:
 		void split_message(const char *message, uint64 len, uint32** &out, uint64 &out_len, uint16 block_size);
+		void split_file(const char *file_name, uint32** &out, uint64 &out_len, uint16 block_size);
 };
 
 #endif
