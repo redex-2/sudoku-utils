9d66edb3aba52300123229f5a8fea2fbc30361ba
redex-2 <redex2mc@gmail.com>
2021-01-11 19:00:03 +0100

    added md5 methonds for loop calculations and init method

--- md-5.cpp
+++ md-5.cpp
@@ -1,1 +1,36 @@
 #include "md-5.hpp"
+
+uint32 _md5::F(uint32 B, uint32 C, uint32 D)
+{
+	return (B&C)|(~B&D);
+}
+
+uint32 _md5::G(uint32 B, uint32 C, uint32 D)
+{
+	return (B&D)|(C&~D);
+}
+
+uint32 _md5::H(uint32 B, uint32 C, uint32 D)
+{
+	return B^C^D;
+}
+
+uint32 _md5::I(uint32 B, uint32 C, uint32 D)
+{
+	return C^(B|~D);
+}
+
+void _md5::init(void)
+{
+	hash[0] = 0x01234567;
+	hash[1] = 0x89abcdef;
+	hash[2] = 0xfedcba98;
+	hash[3] = 0x76543210;
+}
+
+std::string _md5::create(char *message, uint64 len)
+{
+	sstring.str(std::string());
+	_hash::split_message(message, len, data, out_len, block_size);
+	return sstring.str();
+}
--- md-5.hpp
+++ md-5.hpp
@@ -3,7 +3,26 @@
 
 #include "types.hpp"
 #include "hash.hpp"
+#include <sstream>
 
+class _md5:protected _hash
+{
+	private:
+		uint32 hash[4];
+		uint32 block_size = 512;
+		std::stringstream sstring;
+		uint64 out_len;
+		uint32** data;
+		
+		uint32 F(uint32, uint32, uint32);
+		uint32 G(uint32, uint32, uint32);
+		uint32 H(uint32, uint32, uint32);
+		uint32 I(uint32, uint32, uint32);
+		void init(void);
 
+	public:
+		std::string create(char*, uint64);
+
+};
 
 #endif
