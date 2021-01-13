c50d513268cc6919eae5d2ccb4cca8909af47e44
redex-2 <redex2mc@gmail.com>
2021-01-13 11:57:43 +0100

    added working md5 module

--- md-5.hpp
+++ md-5.hpp
@@ -8,17 +8,25 @@
 class _md5:protected _hash
 {
 	private:
-		uint32 hash[4];
-		uint32 block_size = 512;
-		std::stringstream sstring;
-		uint64 out_len;
+		uint16 i;
 		uint32** data;
+		uint32 hash[4];
+		uint32 working[6];
+		uint32 word[16];
+		uint32 block_size = 512;
+		uint64 out_len, j;
+		static const uint32 k[64];
+		static const uint16 s[64];
+		std::stringstream sstring;
 		
+		uint32 rotate(uint32, uint16);
+		uint32 permutation(uint32);
 		uint32 F(uint32, uint32, uint32);
 		uint32 G(uint32, uint32, uint32);
 		uint32 H(uint32, uint32, uint32);
 		uint32 I(uint32, uint32, uint32);
 		void init(void);
+		void rewriting(void);
 
 	public:
 		std::string create(char*, uint64);
