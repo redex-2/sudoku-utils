22cc9541f6f813d03428ceef5be644cf43e2e19b
redex-2 <redex2mc@gmail.com>
2021-01-13 12:31:06 +0100

    added comment

--- testing-main.cpp
+++ testing-main.cpp
@@ -17,6 +17,8 @@ int main()
 	unsigned long long key = 0x133457799BBCDFF1;
 	
 	unsigned long long v = des.encrypt(v0, key);
+	
+	//des test
 	if(v==v1)std::cout<<"encryption ok!\t"<<std::hex<<v<<std::endl;
 	else std::cout<<"encryption not work!\t"<<std::hex<<v<<std::endl;
 	v = des.decrypt(v1, key);
@@ -24,11 +26,13 @@ int main()
 	if(v==v0)std::cout<<"decryption ok!\t"<<std::hex<<v<<std::endl;
 	else std::cout<<"decryption not work!\t"<<std::hex<<v<<std::endl;
 	
+	//sha256 test
 	std::string sha0="8fc32d0d087d03115b7b200631109d66a60e052e434aa1609971130fa24d6d25";
 	std::string sha = sha256.create((char*)"worldogjnosafworldogjnosafworldogjnosafworldogjnosafworldogjnosafworldogjnosafworldogjnosafworldogjnosaf", 104);
 	if(sha==sha0)std::cout<<"sha256 ok!\t"<<std::hex<<sha<<std::endl;
 	else std::cout<<"sha256 not work!\t"<<std::hex<<sha<<std::endl;
 	
+	//md5 test
 	std::string md0="9e107d9d372bb6826bd81d3542a419d6";
 	std::string md = md5.create((char*)"The quick brown fox jumps over the lazy dog", 43); 
 	if(md0==md)std::cout<<"md5 ok!\t"<<std::hex<<md<<std::endl;
