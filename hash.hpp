5c5b7b2a09206b5a59dfb6ad56115330614bc254
redex-2 <redex2mc@gmail.com>
2021-01-16 19:50:39 +0100

    delete input permutatrion and create split message for LE

--- hash.hpp
+++ hash.hpp
@@ -5,6 +5,17 @@
 
 class _hash
 {
+	private:
+		uint64 i;
+	protected:
+		void split_message(const char *message, uint64 len, uint32** &out, uint64 &out_len, uint16 block_size);
+		void split_file(const char *file_name, uint32** &out, uint64 &out_len, uint16 block_size);
+};
+
+class _reverse_hash
+{
+	private:
+		uint64 i;
 	protected:
 		void split_message(const char *message, uint64 len, uint32** &out, uint64 &out_len, uint16 block_size);
 		void split_file(const char *file_name, uint32** &out, uint64 &out_len, uint16 block_size);
