cef6b4a1635d6fe1d7097572c2e3a388a2bc667c
redex-2 <redex2mc@gmail.com>
2021-01-17 10:42:54 +0100

    added body od prime class for uint numbet < 32 bit

--- prime.hpp
+++ prime.hpp
@@ -6,14 +6,18 @@
 class _prime
 {
 	private:
-		uint64 sqrt(uint64 &);
-		bool miller_rabin(uint64 &);
-		bool standard(uint64 &);
+		uint16 m = 2;//0=standard 1-miller_rabin 2-auto
+		uint32 k = 100;
+		uint64 result, i, d, a, x;
+		uint64 sqrt(uint64);
+		uint64 power(uint64 base, uint64 exponent, uint64 mod);
+		bool miller_rabin(uint32 &);
+		bool standard(uint32 &);
 	public:
+		void precision(uint32);
 		void method(uint16);
-		void precision(uint64);
-		uint16 check(uint64);
-		uint64 next(uint64);
+		uint16 check(uint32);
+		uint32 next(uint32);
 };
 
 #endif
