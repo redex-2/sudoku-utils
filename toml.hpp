3ed76bc71afbee06e9ef76d9034b471f1b52dd31
redex-2 <redex2mc@gmail.com>
2021-01-18 21:25:02 +0100

    update hpp

--- toml.hpp
+++ toml.hpp
@@ -1,6 +1,7 @@
 #ifndef toml_hpp
 #define toml_hpp
 
+#include "types.hpp"
 #include <cstring>
 #include <fstream>
 
@@ -9,15 +10,19 @@ class _toml
 	private:
 		std::string file = "";
 		bool is_setting_up = 0;
-		std::string temp;
+		std::string s;
+		std::string k;
 
 	public:
 		_toml(void);
 		_toml(std::string filename);
 		int setup(std::string filename);
+		bool section_list(std::string *);
 		bool section(std::string section);
 		bool section(void);
+		bool key_list(std::string *);
 		bool key(std::string key);
+		bool value_list(uint64 *);
 		bool get(std::string &data);
 };
 
